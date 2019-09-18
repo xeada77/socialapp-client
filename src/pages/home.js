010aecb1015ce431c327dafb3f19fa77716c5365
xeada77 <xeada77@gmail.com>
2019-09-18 19:20:56 +0200

    Creados varios componentes y estilos

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
+
+import Scream from './../components/Scream';
 
 class home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    async componentDidMount() {
+        try {
+            const screams = await axios.get('/screams');
+            this.setState({ screams: screams.data });
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
     render() {
+        let recentScreamsMarkup = this.state.screams ?
+            (this.state.screams.map(scream => { return <Scream scream={scream} key={scream.screamId} /> })) :
+            <p>Loading....</p>
+
         return (
-            <div>
-                <h1>Home Page</h1>
-            </div>
+            <Grid container spacing={4}>
+                <Grid item sm={8} xs={12}>
+                    {recentScreamsMarkup}
+                </Grid>
+                <Grid item sm={4} xs={12}>
+                    <p>Profile..</p>
+                </Grid>
+            </Grid>
         )
     }
 }
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,13 +1,72 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import withStyles from '@material-ui/core/styles/withStyles';
+import PropTypes from 'prop-types';
+import AppIcon from './../images/icon.png';
+
+// MUI Stuff
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+
+const styles = {
+    form: {
+        textAlign: 'center'
+    },
+    image: {
+        margin: '20px auto 20px auto'
+    }
+
+}
+
+
 
 class login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            email: '',
+            password: '',
+            loading: false,
+            errors:{},
+        }
+    }
+
+    handleSubmit = (event) => {
+        console.log('hi');
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+
     render() {
+        const { classes } = this.props;
         return (
-            <div>
-                <h1>Login Page</h1>
-            </div>
+            <Grid container className={classes.form}>
+                <Grid item sm />
+                <Grid item sm>
+                    <img src={AppIcon} alt="Icon" className={classes.image} />
+                    <Typography variant="h2" className={classes.pageTitle}>
+                        Login
+                    </Typography>
+                    <form noValidate onSubmit={this.handleSubmit}>
+                        <TextField id="email" name="email" label="Email" className={classes.textField} value={this.state.email}
+                            onChange={this.handleChange} fullWidth />
+                        <TextField id="password" name="password" label="Password" type="password" className={classes.textField} value={this.state.password}
+                            onChange={this.handleChange} fullWidth />
+                    </form>
+                </Grid>
+                <Grid item sm />
+           </Grid>
         )
     }
 }
 
-export default login
+login.propTypes = {
+    classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(login);
