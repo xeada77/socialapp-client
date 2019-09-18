7a5c2a027ea6c02d972253cfdee1389c83f73b11
xeada77 <xeada77@gmail.com>
2019-09-18 20:40:58 +0200

    Login dev

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
+import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import AppIcon from './../images/icon.png';
+import axios from 'axios';
 
 // MUI Stuff
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress'
 
 const styles = {
     form: {
@@ -14,8 +18,26 @@ const styles = {
     },
     image: {
         margin: '20px auto 20px auto'
+    },
+    pageTitle: {
+        margin: '10px auto 10px auto'
+    },
+    textField: {
+        margin: '10px auto 10px auto'
+    },
+    button: {
+        marginTop: 20,
+        marginBottom: 20,
+        position: 'relative'
+    },
+    customError: {
+        color: 'red',
+        fontSize: '0.8rem',
+        marginTop: 10
+    },
+    progress: {
+        position: 'absolute'
     }
-
 }
 
 
@@ -31,8 +53,28 @@ class login extends Component {
         }
     }
 
-    handleSubmit = (event) => {
-        console.log('hi');
+    handleSubmit = async (event) => {
+        event.preventDefault();
+        this.setState({ loading: true });
+        const userData = {
+            email: this.state.email,
+            password: this.state.password,
+        }
+        
+        try {
+            const response = await axios.post('/login', userData);
+            console.log(response.data);
+            this.setState({ loading: false });
+            this.props.history.push('/');
+        } catch (err) {
+            console.log(err.response.data);
+            this.setState({
+                errors: err.response.data,
+                loading: false
+            });
+            //console.log(this.state);
+        }
+        
     }
 
     handleChange = (event) => {
@@ -44,6 +86,8 @@ class login extends Component {
 
     render() {
         const { classes } = this.props;
+        const { errors, loading } = this.state;
+
         return (
             <Grid container className={classes.form}>
                 <Grid item sm />
@@ -53,11 +97,51 @@ class login extends Component {
                         Login
                     </Typography>
                     <form noValidate onSubmit={this.handleSubmit}>
-                        <TextField id="email" name="email" label="Email" className={classes.textField} value={this.state.email}
-                            onChange={this.handleChange} fullWidth />
-                        <TextField id="password" name="password" label="Password" type="password" className={classes.textField} value={this.state.password}
-                            onChange={this.handleChange} fullWidth />
+                        <TextField
+                            id="email"
+                            name="email"
+                            label="Email"
+                            helperText={errors.email}
+                            error={errors.email? true: false}
+                            className={classes.textField}
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                            fullWidth
+                            autoFocus={true} />
+                        <TextField
+                            id="password"
+                            name="password"
+                            label="Password"
+                            type="password"
+                            helperText={errors.password}
+                            error={errors.password ? true: false}
+                            className={classes.textField}
+                            value={this.state.password}
+                            onChange={this.handleChange}
+                            fullWidth />
+                        
+                        {errors.general && (
+                            <Typography
+                                variant="body2"
+                                className={classes.customError}
+                            >
+                                {errors.general}
+                            </Typography>
+                        )}
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            color="primary"
+                            className={classes.button}
+                            disabled={loading}
+                        >
+                            Login
+                        {loading && (
+                            <CircularProgress size={30} className={classes.progress}/>)
+                            }
+                        </Button>                        
                     </form>
+                    <small>dont have an account? sign up <Link to="/signup">here</Link></small>
                 </Grid>
                 <Grid item sm />
            </Grid>
