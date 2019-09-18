1afbcf325c2bc11aa0ccec022181867dd35b02c5
Luis Martinez <luismartq@gmail.com>
2019-09-18 14:23:11 +0200

    Añadidos componentes y algunas paginas

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Link from 'react-router-dom/Link';
+
+// Material-UI Stuff
+import Appbar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+
+
+
+class Navbar extends Component {
+    render() {
+        return (
+            <Appbar position="fixed">
+                <Toolbar className="nav-container">
+                    <Button color="inherit" component={Link} to="/login">Login</Button>
+                    <Button color="inherit" component={Link} to="/signup">SignUp</Button>
+                    <Button color="inherit" component={Link} to="/">Home</Button>
+                </Toolbar>
+            </Appbar>
+        )
+    }
+}
+
+export default Navbar
